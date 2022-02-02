2dc710c115b0ed9da4bee1e3d85ef7925a5c920d
thaeki <pedrokopsch@gmail.com>
2022-02-02 15:50:53 -0300

    feat: flask CORS added

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from app import routes
 
 def create_app():
 
     app = Flask(__name__, static_folder=None)
+    CORS(app)
     routes.init_app(app)
 
     return app
